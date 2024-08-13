d7d83690c3bd274439ad2c464f0af9a07769b95b
Daniel Santamaria <santamaria.daniel316@gmail.com>
2024-08-12 20:49:18 -0700

    Setup Landing Page

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDyABumdf2e8vP3EU6Cyy0Y4RRiAqsE9as",
+  authDomain: "flashcardsaas-68271.firebaseapp.com",
+  projectId: "flashcardsaas-68271",
+  storageBucket: "flashcardsaas-68271.appspot.com",
+  messagingSenderId: "862390801973",
+  appId: "1:862390801973:web:f7197d82f337c3bb5a037c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
