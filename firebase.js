72b98d40462dc027e45496d9d3b9807650340718
Daniel Santamaria <santamaria.daniel316@gmail.com>
2024-08-14 17:09:39 -0700

    continue generate page

--- firebase.js
+++ firebase.js
@@ -1,6 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getFirebase} from "firebase/firestore"
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,7 +20,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirebase(app)
+const app = firebase.initializeApp(firebaseConfig);
+const db = app.firestore()
 
 export{db}
